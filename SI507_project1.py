69fb3560986536748f44396b128d4e6cacd9f345
Maggie Davidson <jmagdavidson@gmail.com>
2019-02-03 14:14:42 -0500

    Adding routes for pound and yuan and creating a .gitignore file

--- SI507_project1.py
+++ SI507_project1.py
@@ -21,10 +21,30 @@ def createBankSpecified(bankname, specific_currency):
 
 @app.route('/dollar/<amt>')
 def showDollar(amt):
-    dollar = classes.Dollar(int(amt))
-    return "<h1>{} Dollar</h1>".format(dollar.value)
+    try:
+        amt = int(amt)
+        dollar = classes.Dollar(int(amt))
+        return "<h1>{} Dollar</h1>".format(dollar.value)
+    except:
+        return "<h1>Please use a valid integer for the dollar amount</h1>"
 
+@app.route('/pound/<amt>')
+def showPound(amt):
+    try:
+        amt = int(amt)
+        pound = classes.Pound(int(amt))
+        return "<h1>{} Pound</h1>".format(pound.value)
+    except:
+        return "<h1>Please use a valid integer for the pound amount</h1>"
 
+@app.route('/yuan/<amt>')
+def showYuan(amt):
+    try:
+        amt = int(amt)
+        yuan = classes.Yuan(int(amt))
+        return "<h1>{} Yuan</h1>".format(yuan.value)
+    except:
+        return "<h1>Please use a valid integer for the yuan amount</h1>"
 
 if __name__ == '__main__':
     app.run()
