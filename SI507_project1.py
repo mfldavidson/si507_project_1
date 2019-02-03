ed0378b77fc9106e9641717ba7f8b8e78648c839
Maggie Davidson <jmagdavidson@gmail.com>
2019-02-03 13:52:19 -0500

    Adding routes for homepage, creating a bank, and creating a dollar

--- SI507_project1.py
+++ SI507_project1.py
@@ -5,20 +5,24 @@ import lab3_code as classes
 app = Flask(__name__)
 
 # Routes
-
 @app.route('/')
-def welcome(): # Route function names can be anything unique
-    return '<h1>Welcome to the banking application!</h1>' # Route functions can return plain old text, or text in correctly formatted HTML (or the return values of many special functions we haven't looked at yet)
+def welcome():
+    return '<h1>Welcome to the banking application!</h1>'
 
-# Something else to note is that the functions don't get INVOKED the way you may be used to. Running the application and *going to a URL that matches the path in the @app.route() business IS what runs that function!
+@app.route('/bank/<bankname>')
+def createBankDefault(bankname):
+    bank = classes.Bank(bankname, classes.Currency)
+    return '<h1>Welcome to {}</h1>'.format(bank.name)
 
-@app.route('/user/<yourname>')
-def hello_name(yourname): # Note how the word in < > carats / angle-brackets is the SAME as the input to the function - that means the URL is what sends the input along
-    return 'Hello {}'.format(yourname)
+@app.route('/bank/<bankname>/<specific_currency>')
+def createBankSpecified(bankname, specific_currency):
+    bank = classes.Bank(bankname, specific_currency)
+    return '<h1>Welcome to {}</h1>'.format(bank.name)
 
-@app.route('/showvalues/<name>/<greeting>')
-def showvalues(name, greeting): # Check this out -- what's going on here? A useful pattern to think about.
-    return "{}, {}!".format(greeting, name)
+@app.route('/dollar/<amt>')
+def showDollar(amt):
+    dollar = classes.Dollar(int(amt))
+    return "<h1>{} Dollar</h1>".format(dollar.value)
 
 
 
